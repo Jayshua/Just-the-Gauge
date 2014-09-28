4a81b32ae44cd801c9c56b8acf7ccc554375ee2f
yajtheman <yajtheman@gmail.com>
2014-09-28 13:55:27 -0500

    Switched to millisecond duration rather than iteration

--- Guage.js
+++ Guage.js
@@ -1,31 +1,75 @@
+/*
+ *
+ * TERMS OF USE - EASING EQUATIONS
+ * 
+ * Open source under the BSD License. 
+ * 
+ * Copyright Â© 2001 Robert Penner
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of 
+ * conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list 
+ * of conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution.
+ * 
+ * Neither the name of the author nor the names of contributors may be used to endorse 
+ * or promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE. 
+ *
+ */
+
+
 var Guage = function(canvas, options) {
 	options = options || {};
+
 	var defaultOptions = {
 		color: "lightblue",
 		bgColor: "#eee",
-		clockwise: true,
-		value: Math.PI*1.5,
-		rotation: -Math.PI/2,
+		clockwise: false,
+		value: Math.PI*1.2,
+		rotation: 0,
 		radius: 50,
-		stepSize: Math.PI/20
+		duration: 1000
 	};
+
 	for( var key in defaultOptions ) {
 		if( typeof options[key] == "undefined") options[key] = defaultOptions[key];
 		this[key] = options[key];
 	}
 
+
+
     this.radians = 0;
     this.destinationRadians = options.value;
 
     this.ctx = canvas.getContext("2d");
+    this.iterationCount = 0;
+
+    this.startTime = Date.now();
+
+    this.render();
 };
+
 
 Guage.prototype.drawArc = function(startDegree, endDegree) {
 	var ctx = this.ctx;
 	var canvas = ctx.canvas;
 
-	startDegree += this.rotation;
-	endDegree += this.rotation;
+	startDegree += -Math.PI/2 + this.rotation;
+	endDegree += -Math.PI/2 + this.rotation;
 
 	ctx.beginPath();
 	ctx.arc(canvas.width/2, canvas.height/2, this.radius, startDegree, endDegree, this.clockwise);
@@ -33,10 +77,14 @@ Guage.prototype.drawArc = function(startDegree, endDegree) {
 
 };
 
-Guage.prototype.update = function() {
-	if( this.radians >= this.destinationRadians - this.stepSize &&
-		this.radians <= this.destinationRadians + this.stepSize ) {
+Guage.prototype.ease = function(currentIteration, startValue, changeInValue, totalIterations) {
+	return -changeInValue * (Math.pow(currentIteration / totalIterations - 1, 4) - 1) + startValue;
+};
 
+Guage.prototype.render = function() {
+	var now = Date.now();
+
+	if( now - this.startTime > this.duration ) {
 		return;
 	}
 
@@ -52,7 +100,9 @@ Guage.prototype.update = function() {
     ctx.strokeStyle = this.color;
     this.drawArc(0, this.radians);
 
-    this.radians += this.stepSize;
+	this.radians = this.ease( now - this.startTime, 0, this.destinationRadians, this.duration);
+
+	window.requestAnimationFrame( this.render.bind(this) );
 };
 
 
@@ -62,5 +112,4 @@ Guage.prototype.update = function() {
 	document.body.appendChild(canvas);
 
 	guage = new Guage(canvas);
-	setInterval( guage.update.bind(guage), 50 );
 })();
